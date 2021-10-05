5f39b1af7f8d308c85cd059d59a01d7b367a72c2
Carter Wagner <cjwagn1@gmail.com>
2021-10-05 00:18:10 -0500

    everything

--- src/components/sections/Layout.tsx
+++ src/components/sections/Layout.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import Header from "./Header";
 import FAQ from "./FAQ";
 import Schedule from "./Schedule";
+import Sponsor from "./Sponsor";
+import Partner from "./Partner";
+import Team from "./Team";
 import Navbar from "../common/Navbar/Navbar";
 import styled, { AnyStyledComponent } from "styled-components";
+import Background from "../../images/background2.png";
 
 const FlexContainer: AnyStyledComponent = styled.div`
   display: flex;
@@ -15,15 +19,24 @@ const FlexCenter: AnyStyledComponent = styled.div`
   justify-content: center;
   display: flex;
   width: 100%;
+  background-image: url(${Background});
+  background-size: cover;
 `;
 
 const Constraint: AnyStyledComponent = styled.div`
   display: flex;
-  width: 1000px;
 `;
 const SectionDiv: AnyStyledComponent = styled.section`
-  padding-bottom: 8%;
-  padding-top: 8%;
+  padding-bottom: 14%;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding-top: 3%;
+
+  @media (max-width: 1000px) {
+    padding-bottom: 7%;
+  }
 `;
 export default () => {
   return (
@@ -31,9 +44,10 @@ export default () => {
       <Navbar />
 
       <FlexCenter>
+        <div style={{ height: "45px" }}> </div>
         <Constraint>
           <FlexContainer>
-            <SectionDiv id="header">
+            <SectionDiv style={{ paddingTop: "20%" }} id="header">
               <Header />
             </SectionDiv>
 
@@ -43,6 +57,18 @@ export default () => {
 
             <SectionDiv id="faq">
               <FAQ />
+            </SectionDiv>
+
+            <SectionDiv id="sponsor">
+              <Sponsor />
+            </SectionDiv>
+
+            <SectionDiv id="partner">
+              <Partner />
+            </SectionDiv>
+
+            <SectionDiv id="team">
+              <Team />
             </SectionDiv>
           </FlexContainer>
         </Constraint>
--- src/components/sections/Team.tsx
+++ src/components/sections/Team.tsx
@@ -1,9 +1,143 @@
 import React from "react";
-
+import {
+  Bharat,
+  Rachel,
+  Ryan,
+  Scott,
+  Sabrina,
+  Riley,
+  Jack,
+  Himnish,
+  Eddie,
+  Dominic,
+  Carter,
+  Bradley,
+  Billie,
+  Colton,
+} from "../../images/Images";
 import styled, { AnyStyledComponent } from "styled-components";
 
-const Example: AnyStyledComponent = styled.div``;
+const Title: AnyStyledComponent = styled.div`
+  font-size: 4.5vw;
+  color: #f47525;
+  padding-bottom: 20px;
+  text-align: center;
+  @media screen and (max-width: 1000px) {
+    font-size: 8vw;
+  }
+`;
 
+const FlexCenter: AnyStyledComponent = styled.div`
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const Grid: AnyStyledComponent = styled.div`
+  display: grid;
+  max-width: 1200px;
+  margin: 0 auto;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  grid-gap: 40px;
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 500px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+const GridItem: AnyStyledComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const GridImage: AnyStyledComponent = styled.img`
+  height: 100px;
+  width: 100px;
+  border-radius: 50%;
+  margin: auto;
+`;
+
+const Name: AnyStyledComponent = styled.div`
+  display: flex;
+  font-family: "Poppins", sans-serif;
+  margin: auto;
+  color: white;
+`;
 export default () => {
-  return <Example>This is an example component</Example>;
+  return (
+    <div>
+      <FlexCenter>
+        <Title>TEAM</Title>
+      </FlexCenter>
+
+      <Grid>
+        <GridItem>
+          <GridImage src={Bharat}></GridImage>
+
+          <Name style={{ fontSize: "15.5px" }}>Bharat Sreekrishnavilas</Name>
+        </GridItem>
+
+        <GridItem>
+          <GridImage src={Eddie}></GridImage>
+
+          <Name>Edward Keith</Name>
+        </GridItem>
+        <GridItem>
+          <GridImage src={Riley}></GridImage>
+          <Name>Riley Fuller</Name>
+        </GridItem>
+        <GridItem>
+          <GridImage src={Sabrina}></GridImage>
+          <Name>Sabrina McRoberts</Name>
+        </GridItem>
+        <GridItem>
+          <GridImage src={Rachel}></GridImage>
+          <Name>Rachel Liang</Name>
+        </GridItem>
+        <GridItem>
+          <GridImage src={Billie}></GridImage>
+          <Name>Billie Huang</Name>
+        </GridItem>
+        <GridItem>
+          <GridImage src={Jack}></GridImage>
+          <Name>Jack Stork</Name>
+        </GridItem>
+
+        <GridItem>
+          <GridImage src={Ryan}></GridImage>
+          <Name>Ryan Duffendack</Name>
+        </GridItem>
+        <GridItem>
+          <GridImage src={Bradley}></GridImage>
+          <Name>Bradley Moore</Name>
+        </GridItem>
+        <GridItem>
+          <GridImage src={Colton}></GridImage>
+          <Name>Colton Jacobson</Name>
+        </GridItem>
+        <GridItem>
+          <GridImage src={Scott}></GridImage>
+          <Name>Scott Connor</Name>
+        </GridItem>
+        <GridItem>
+          <GridImage src={Carter}></GridImage>
+          <Name>Carter Wagner</Name>
+        </GridItem>
+        <GridItem>
+          <GridImage src={Himnish}></GridImage>
+          <Name>Himnish Sapkota</Name>
+        </GridItem>
+        <GridItem>
+          <GridImage src={Dominic}></GridImage>
+          <Name>Dominic Alberico</Name>
+        </GridItem>
+      </Grid>
+    </div>
+  );
 };
